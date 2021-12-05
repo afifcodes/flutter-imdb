abdfe8115223b12063471287933baa27482e1e98
afifudinn <work.afifudin@gmail.com>
2021-12-05 12:01:15 +0700

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,178 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const MyApp(),
+      theme: ThemeData(fontFamily: 'Urbanist'),
+    ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  TextStyle textStyle = const TextStyle(color: Colors.white);
+  List<String> categories = ['Action', 'Adventure', 'Fantasy'];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  //////////////////////////////
+  /// @withflutter
+  ///
+  /// afifudin.vercel.app
+  //////////////////////////////
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.black,
+      body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            expandedHeight: 500,
+            backgroundColor: Colors.black,
+            flexibleSpace: FlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              background: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/shangchi.jpg'),
+                        fit: BoxFit.cover,
+                        alignment: Alignment.topCenter)),
+                child: Container(
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.black, Colors.transparent],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(30),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Shang-Chi',
+                          style: textStyle.copyWith(
+                            fontSize: 42,
+                            fontWeight: FontWeight.w800,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Text('and The Legend of The Ten Rings',
+                            style: textStyle.copyWith(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                            ))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            SizedBox(
+              height: 30,
+              child: ListView.builder(
+                  itemCount: categories.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      margin:
+                          EdgeInsets.only(left: index == 0 ? 30 : 0, right: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(99),
+                          border: Border.all(color: Colors.white, width: 1)),
+                      child: Center(
+                        child: Text(categories[index], style: textStyle),
+                      ),
+                    );
+                  }),
             ),
-          ],
-        ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 30, left: 30, right: 30, bottom: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      'Shang-Chi, the master of weaponry-based Kung Fu, is forced to confront his past after being drawn into the Ten Rings organization.',
+                      style: textStyle),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text('Director',
+                      style: textStyle.copyWith(
+                          fontWeight: FontWeight.w700, fontSize: 18)),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text('Destin Daniel Cretton', style: textStyle),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text('Writters',
+                      style: textStyle.copyWith(
+                          fontWeight: FontWeight.w700, fontSize: 18)),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text(
+                      'Dave Callaham (screenplay by) . Destin Daniel Cretton (screenplay by) . Andrew Lanham(screenplay by)',
+                      style: textStyle),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text('Stars',
+                      style: textStyle.copyWith(
+                          fontWeight: FontWeight.w700, fontSize: 18)),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Text('Simu Liu, Awkwafina, Tony Chiu-Wai Leung',
+                      style: textStyle),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  Text('Photos',
+                      style: textStyle.copyWith(
+                          fontWeight: FontWeight.w700, fontSize: 18)),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 140,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 200,
+                    margin:
+                        EdgeInsets.only(left: index == 0 ? 30 : 0, right: 15),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/${index + 1}.jpg'),
+                            fit: BoxFit.cover)),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+          ]))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
